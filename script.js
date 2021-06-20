014e046a869b4d6f079dde43459ce02086d50814
Sobia Ahmad <63017839+sobia-ahmad@users.noreply.github.com>
2021-06-20 14:55:54 +0100

    JS done

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-
+// Assigning variables to targeted classes for units and buttons
 let addSeconds = document.querySelector('.seconds');
 let addTens = document.querySelector('.tens');
 let startButton = document.querySelector('.start-button');
 let pauseButton = document.querySelector('.pause-button');
 let resetButton = document.querySelector('.reset-button');
 
+// Setting units to 00
 let seconds = 00;
 let tens = 00;
 
-let duration = setInterval;
+// Break stored for setInterval
+let breakStall = setInterval;
 
-
+// Start timer function with conditions for how tens/seconds should appear, changes innerText
 function startTimer() {
   tens++;
   if (tens <= 9) {
-    addTens.innerText = `0${tens}`;
+    addTens.innerText = `0${tens}`; // template literals used for strings and JS
   } 
   if (tens > 9) {
     addTens.innerText = tens;
@@ -30,18 +32,22 @@ function startTimer() {
    }
 }
 
+// onclick events applied to the start, pause and reset buttons
+
+// This will begin the timer
 startButton.onclick = function() {
-  duration = setInterval(startTimer);
+  breakStall = setInterval(startTimer);
 };
-
+// This will pause the timer
 pauseButton.onclick = function() {
-  clearInterval(duration);
+  clearInterval(breakStall);
 };
 
+// This will reset the timer so tens and seconds are shown as 0 again
 resetButton.onclick = function() {
-  clearInterval(duration);
+  clearInterval(breakStall);
   tens = '00';
   seconds = '00';
   addSeconds.innerText = seconds;
   addTens.innerText = tens;
-}
+} 
